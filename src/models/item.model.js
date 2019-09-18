4d1c143bde42ddab8e5e0eac526cca7a615d5f55
Abdulla Ashraf <abdulla.ashrf@yahoo.com>
2019-09-18 15:14:03 +0200

    .gitignore fix

--- src/models/item.model.js
+++ src/models/item.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const user = 'Abdulla'
-const password = 'MFtwfwi8VFVXDLyh'
+const password = process.env.PASS
 
 mongoose.connect(`mongodb+srv://${user}:${password}@tft-rbx8e.mongodb.net/test?retryWrites=true&w=majority`)
 
